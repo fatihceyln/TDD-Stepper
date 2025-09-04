f5374b0bd4aece9f161b795fe0e35948ae3d7789
Fatih Kilit <45256657+fatihceyln@users.noreply.github.com>
2025-09-04 14:32:33 +0300

    Updates value to match with minimum value when minimum value is set to greater than current value

--- TDDStepper/TDDStepperTests/StepperTests.swift
+++ TDDStepper/TDDStepperTests/StepperTests.swift
@@ -8,8 +8,14 @@
 import XCTest
 
 class Stepper {
-    let value: UInt = 0
-    let minimumValue: UInt = 0
+    var value: UInt = 0
+    var minimumValue: UInt = 0 {
+        didSet {
+            if minimumValue > value {
+                value = minimumValue
+            }
+        }
+    }
     let maximumValue: UInt = 10
     let stepValue: UInt = 1
 }
@@ -22,5 +28,13 @@ class StepperTests: XCTestCase {
         XCTAssertEqual(sut.minimumValue, 0, "Default minimum value")
         XCTAssertEqual(sut.maximumValue, 10, "Default maximum value")
         XCTAssertEqual(sut.stepValue, 1, "Default step value")
+    }
+    
+    func test_setMinimumValue_updatesValueToMinimumValue_whenMinimumValueIsGreaterThanValue() {
+        let sut = Stepper()
+        
+        sut.minimumValue = 5
+        
+        XCTAssertEqual(sut.value, 5, "Expected value to be equal with minimum value")
     }
 }
