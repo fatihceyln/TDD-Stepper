b70a84d3e712360db90c57848fd85e547a61911c
Fatih Kilit <45256657+fatihceyln@users.noreply.github.com>
2025-09-05 11:23:23 +0300

    Requests timer from provider when touch starts

--- TDDStepper/TDDStepper/StepperButton.swift
+++ TDDStepper/TDDStepper/StepperButton.swift
@@ -12,10 +12,25 @@ class StepperButton: UIButton {
         CGSize(width: 32, height: 32)
     }
     
+    typealias TimerCallback = () -> Void
+    typealias TimerProvider = (@escaping TimerCallback) -> Timer?
+    
+    private var timerProvider: TimerProvider?
+    private var timer: Timer?
+    
+    convenience init(timerProvider: @escaping TimerProvider) {
+        self.init()
+        self.timerProvider = timerProvider
+    }
+
     override func layoutSubviews() {
         super.layoutSubviews()
         
         layer.cornerRadius = bounds.width / 2
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        timer = timerProvider?() {}
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
--- TDDStepper/TDDStepperTests/StepperButtonTests.swift
+++ TDDStepper/TDDStepperTests/StepperButtonTests.swift
@@ -17,9 +17,42 @@ class StepperButtonTests: XCTestCase {
         sut.simulateTap()
         XCTAssertEqual(eventCount, 1)
     }
+    
+    func test_requestsTimer_whenTouchStarts() {
+        var timer: TimerSpy?
+        let sut = StepperButton(timerProvider: { callback in
+            timer = TimerSpy(block: callback)
+            return timer!
+        })
+        
+        sut.simulateTouchStart()
+        XCTAssertNotNil(timer)
+    }
+    
+    // MARK: - Helpers
+    private class TimerSpy: Timer {
+        private var block: (() -> Void)?
+        
+        convenience init(block: @escaping () -> Void) {
+            self.init()
+            self.block = block
+        }
+        
+        override func fire() {
+            block?()
+        }
+        
+        override func invalidate() {
+            block = nil
+        }
+    }
 }
 
 extension StepperButton {
+    func simulateTouchStart() {
+        touchesBegan([], with: nil)
+    }
+    
     func simulateTap() {
         touchesEnded([], with: nil)
     }
