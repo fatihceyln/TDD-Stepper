273ad98360005d4ef0d7282c1dda70e091c465ad
Fatih Kilit <45256657+fatihceyln@users.noreply.github.com>
2025-09-10 13:36:54 +0300

    Init does not crash

--- TDDStepper/TDDStepperTests/AcceleratingTimerTests.swift
+++ TDDStepper/TDDStepperTests/AcceleratingTimerTests.swift
@@ -8,64 +8,16 @@
 import XCTest
 
 class AcceleratingTimer {
-    typealias Rate = TimeInterval
-    typealias Rates = [TimeInterval]
-    typealias TimerProvider = (Rate) -> Timer
+    typealias TimerProvider = (TimeInterval) -> Timer
+    private let timers: [TimerProvider]
     
-    private let rates: Rates
-    private let timerProvider: TimerProvider
-    
-    private(set) var rate: Rate?
-    private(set) var timer: Timer?
-    
-    init(rates: Rates, timerProvider: @escaping TimerProvider) {
-        self.rates = rates
-        self.timerProvider = timerProvider
-    }
-    
-    func schedule() {
-        rate = rates.first
-        guard let rate else { return }
-        timer = timerProvider(rate)
+    init(timers: TimerProvider...) {
+        self.timers = timers
     }
 }
 
 class AcceleratingTimerTests: XCTestCase {
-    func test_initWithEmptyRates_doesNotSelectAnyRate() {
-        let sut = AcceleratingTimer(rates: [], timerProvider: { _ in TimerSpy() })
-        
-        XCTAssertNil(sut.rate)
-    }
-    
-    func test_schedule_selectesFirstRate() {
-        let firstRate = 0.1
-        let secondRate = 0.2
-        let sut = AcceleratingTimer(rates: [firstRate, secondRate], timerProvider: { _ in TimerSpy() })
-        
-        sut.schedule()
-        
-        XCTAssertEqual(sut.rate, firstRate)
-    }
-    
-    func test_schedule_requestsTimerWithSelectedRate() {
-        let rate = 0.3
-        let sut = AcceleratingTimer(rates: [rate], timerProvider: { rate in
-            TimerSpy(timeInterval: rate)
-        })
-        
-        sut.schedule()
-        
-        XCTAssertEqual(sut.rate, sut.timer?.timeInterval)
-    }
-    
-    // MARK: - Helpers
-    private final class TimerSpy: Timer {
-        private var _timeInterval: TimeInterval = .zero
-        override var timeInterval: TimeInterval { _timeInterval }
-        
-        convenience init(timeInterval: TimeInterval) {
-            self.init()
-            self._timeInterval = timeInterval
-        }
+    func test_init_doesNotCrash() {
+        let _ = AcceleratingTimer()
     }
 }
