2e52bb782cb3c412a6425a1e4ca92b22a5090b1d
Fatih Kilit <45256657+fatihceyln@users.noreply.github.com>
2025-09-10 18:14:06 +0300

    Default timer specs

--- TDDStepper/TDDStepperTests/FoundationUIActionTimerTests.swift
+++ TDDStepper/TDDStepperTests/FoundationUIActionTimerTests.swift
@@ -9,7 +9,21 @@ import XCTest
 @testable import TDDStepper
 
 final class FoundationUIActionTimer: UIActionTimer {
+    struct TimerSpecs {
+        let timeInterval: TimeInterval
+        let repeats: Bool
+        
+        static func `default`() -> TimerSpecs {
+            TimerSpecs(timeInterval: 1, repeats: true)
+        }
+    }
+    
     private(set) var timer: Timer?
+    let specs: TimerSpecs
+    
+    init(specs: TimerSpecs = .default()) {
+        self.specs = specs
+    }
     
     func schedule(action: @escaping (any UIActionTimer) -> Void) {}
     
@@ -20,5 +34,11 @@ class FoundationUIActionTimerTests: XCTestCase {
     func test_init_doesNotSchduleTimer() {
         let sut = FoundationUIActionTimer()
         XCTAssertNil(sut.timer)
+    }
+    
+    func test_initWithDefaultSpecs() {
+        let sut = FoundationUIActionTimer()
+        XCTAssertEqual(sut.specs.timeInterval, 1)
+        XCTAssertTrue(sut.specs.repeats)
     }
 }
