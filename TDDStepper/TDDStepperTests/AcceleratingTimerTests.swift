50733e070b13abab47d165fc436243b005d82cfd
Fatih Kilit <45256657+fatihceyln@users.noreply.github.com>
2025-09-10 18:32:39 +0300

    Move AcceleratingTimer to prod

--- TDDStepper/TDDStepperTests/AcceleratingTimerTests.swift
+++ TDDStepper/TDDStepperTests/AcceleratingTimerTests.swift
@@ -8,58 +8,6 @@
 import XCTest
 @testable import TDDStepper
 
-class AcceleratingTimer: UIActionTimer {
-    struct InitializedWithEmptyTimers: Error {}
-    
-    typealias AccelerationInterval = TimeInterval
-    
-    private let accelerationInterval: AccelerationInterval
-    private let timers: [UIActionTimer]
-    private(set) var timer: (UIActionTimer)? {
-        didSet { oldValue?.invalidate() }
-    }
-    
-    private var timerIndex = 0
-    
-    init(accelerationInterval: AccelerationInterval, timers: [UIActionTimer]) throws {
-        guard !timers.isEmpty else { throw InitializedWithEmptyTimers() }
-        self.accelerationInterval = accelerationInterval
-        self.timers = timers
-    }
-    
-    func schedule(action: @escaping (UIActionTimer) -> Void) {
-        let startTime = CFAbsoluteTimeGetCurrent()
-        timer = timers[timerIndex]
-        timer?.schedule(action: { [self] _ in
-            action(self)
-            
-            if CFAbsoluteTimeGetCurrent() - startTime >= accelerationInterval {
-                scheduleNextTimer(action: action)
-            }
-        })
-    }
-    
-    func invalidate() {
-        timerIndex = .zero
-        timer?.invalidate()
-    }
-    
-    private func scheduleNextTimer(action: @escaping (UIActionTimer) -> Void) {
-        timerIndex += 1
-        let lastIndex = timers.count
-        guard timerIndex < lastIndex else { return }
-        
-        let startTime = CFAbsoluteTimeGetCurrent()
-        timer = timers[timerIndex]
-        timer?.schedule(action: { [self] _ in
-            action(self)
-            
-            if CFAbsoluteTimeGetCurrent() - startTime >= accelerationInterval {
-                scheduleNextTimer(action: action)
-            }
-        })
-    }
-}
 
 class AcceleratingTimerTests: XCTestCase {
     func test_init_throwsErrorWhenInitializedWithNoTimers() throws {
